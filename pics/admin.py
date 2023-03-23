0b997c7c2faaf43bc9cc7f4a038d8dcd4533578d
Nafismuhammad <nafisnfs717@gmail.com>
2023-03-23 16:03:26 +0530

    first commit

---
+++ pics/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import IMAGE
+# Register your models here.
+admin.site.register(IMAGE)
---
+++ pics/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+class IMAGE(models.Model):
+    Img=models.ImageField()
